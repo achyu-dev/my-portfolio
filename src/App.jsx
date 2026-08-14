5e826d37e48d8bfed6b59ebc746a9f35eecafc80
achyuthss <achyugeorge7744@gmail.com  git config --global user.name achyu-dev:q>
2026-08-14 21:47:57 +0530

    fix: fixed white bar issue in small screen

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Contact from "./components/Contact";
 
 const App = () => {
   return (
-    <div className="min-h-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+    <div className="relative isolate min-h-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed inset-0 -z-10">
         <div className="absolute inset-0 bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
       </div>
